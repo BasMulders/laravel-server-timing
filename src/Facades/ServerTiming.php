211d232abc9d5ceba7ff368df9b9a5197b9e5d7f
Sebastian Mulders <sebastian@jongensvantechniek.nl>
2020-02-07 10:22:28 +0100

    Added annotations to ServerTiming Facade for IDE auto completion

--- src/Facades/ServerTiming.php
+++ src/Facades/ServerTiming.php
@@ -4,6 +4,19 @@ namespace BeyondCode\ServerTiming\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static addMetric(string $metric)
+ * @method static hasStartedEvent(string $key): bool
+ * @method static measure(string $key)
+ * @method static start(string $key)
+ * @method static stop(string $key)
+ * @method static stopAllUnfinishedEvents()
+ * @method static setDuration(string $key, $duration)
+ * @method static getDuration(string $key)
+ * @method static events(): array
+ *
+ * @see \BeyondCode\ServerTiming\ServerTiming
+ */
 class ServerTiming extends Facade
 {
     protected static function getFacadeAccessor()
